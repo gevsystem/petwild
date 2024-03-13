0e0c56b1924af4fd6efc8e52dd1da6d74769d731
gevsystem <163346182+gevsystem@users.noreply.github.com>
2024-03-13 19:41:05 -0300

    create frontClass.php
    create frontClass.php

--- class/frontClass.php
+++ class/frontClass.php
@@ -1,1 +1,17 @@
+<?php
+    require "app/connect/cone.php";
+    class ConsultaF extends DAO
+{
+  public function getConsulta()
+  {
+    parent::__construct();
+  }
 
+  public function consulta($sql = "", $data = array())
+  {
+    $query = $this->conect->prepare($sql);
+    $query->execute($data);
+    $res = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $res;
+  }
+}
